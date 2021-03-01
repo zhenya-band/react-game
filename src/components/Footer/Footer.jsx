b2a0e3cf6b0d8171f9feaa938425ad9e2f5819e2
Yauheni Bandyukevich <zhenya.bandyukevich@mail.ru>
2021-03-02 02:28:51 +0300

    feat: add links in footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import { GithubOutlined } from '@ant-design/icons';
+import rssLogo from './../../assets/rss-logo.svg';
 import './Footer.css';
 
 const Footer = () => {
   return (
     <div className='footer'>
-      <div className="footer-inner">
-        footer
+      <div className='footer-inner'>
+        <a href='https://github.com/zhenya-band'>
+          <GithubOutlined style={{ fontSize: '37px' }} />
+        </a>
+        <div className='footer-year'>2021</div>
+        <a href='https://rs.school/js/' className='footer-logo'>
+          <img className='footer-logo__img' src={rssLogo} alt='logo'/>
+        </a>
       </div>
     </div>
   );
